e1e755847c67eef94207785310a21189fcf50447
Dmitry <kererelore14@gmail.com>
2026-05-11 17:28:32 +0300

    update: added destop hero addaptiwity

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,6 +6,8 @@ import Elipse from "../elipse/Elipse"
 import academicIcon from "../../assets/icons/academic-icon.png"
 import medalIcon from "../../assets/icons/medal-icon.png"
 import layersIcon from "../../assets/icons/layers-icon.png"
+import image1 from "../../assets/images/image1.png"
+import image2 from "../../assets/images/image2.png"
 import "./Hero.css"
 const Hero = () => {
     return (
@@ -13,31 +15,41 @@ const Hero = () => {
             <div className="hero container  ">
 
                 <Header></Header>
-                <div className="hero__content">
-                    <div className="hero__tags">
-                        <div className="tag br-sm">
-                            <img src={medalIcon} alt="" />
-                            <p>Сертифікат про навчання</p></div>
-                        <div className="tag br-sm">
-                            <img src={academicIcon} alt="" />
-                            <p>IT-професія</p></div>
-                        <div className="tag br-sm">
-                            <img src={layersIcon} alt="" />
-                            <p>Портфоліо проектів</p></div>
+                <div className="hero__wrapper">
+                    <div className="hero__content">
+                        <div className="hero__tags">
+                            <div className="tag br-sm">
+                                <img src={medalIcon} alt="" />
+                                <p>Сертифікат про навчання</p></div>
+                            <div className="tag br-sm">
+                                <img src={academicIcon} alt="" />
+                                <p>IT-професія</p></div>
+                            <div className="tag br-sm">
+                                <img src={layersIcon} alt="" />
+                                <p>Портфоліо проектів</p></div>
+                        </div>
+
+                        <div>
+                            <div className="hero__text">
+                                <h2 className="hero__content-title">Школа програмування для дітей від 5 років</h2>
+                                <p className="hero__content-desc">З IT Paradigma ваша дитина вже на перших заняттях створить власну гру, програму або сайт. </p>
+                            </div>
+                            <div className="btns hero_btns">
+                                <Button text={'Записатися'} classes={'btn-course step-1 hero-mobile-shadow-btn'} />
+                                <Button text={'Обрати курс'} classes={'btn-course step-2 hero-desctop-btn'} />
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="hero__text">
-                        <h2 className="hero__content-title">Школа програмування для дітей від 5 років</h2>
-                        <p className="hero__content-desc">З IT Paradigma ваша дитина вже на перших заняттях створить власну гру, програму або сайт. </p>
+                    <div className="hero__wrapper-imgs">
+                        <img src={image1} alt=""/>
+                        <img src={image2} alt=""/>
                     </div>
                 </div>
 
 
 
-                <div className="btns hero_btns">
-                    <Button text={'Записатися'} classes={'btn-course step-1 hero-mobile-shadow-btn'} />
-                    <Button text={'Обрати курс'} classes={'btn-course step-2 hero-desctop-btn'} />
-                </div>
+
                 <Elipse parametr='glow-blue hero-1'></Elipse>
                 <Elipse parametr='glow-green hero-2'></Elipse>
                 <Elipse parametr='glow-blue hero-3'></Elipse>
